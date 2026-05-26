97f605ce45470fceda4a694a3778a9c1de5b08cf
Leona <zur0037@vsb.cz>
2026-05-26 11:32:24 +0300

    loading shards per rank in batches

--- modules/parquet_loader.py
+++ modules/parquet_loader.py
@@ -1,5 +1,7 @@
 import pyarrow.parquet as pq
 import pandas as pd
+from typing import Generator # for batch shard
+
 def load_sharded_parquet(path: str, rank: int, world_size: int) -> pd.DataFrame:
     # load metadata
     parquet_file = pq.ParquetFile(path)
@@ -13,3 +15,31 @@ def load_sharded_parquet(path: str, rank: int, world_size: int) -> pd.DataFrame:
 
     table = parquet_file.read_row_groups(my_row_groups)
     return table.to_pandas()
+
+def batch_sharded_parquet(
+    path: str, rank: int, world_size: int, groups_per_batch: int = 1
+) -> Generator[pd.DataFrame, None, None]:
+    """
+    Generator loads batches for rank
+    groups_per_batch: How many Row Groups goes to one batch.
+    """
+    parquet_file = pq.ParquetFile(path)
+    total_row_groups = parquet_file.num_row_groups
+    my_row_groups = [i for i in range(total_row_groups) if i % world_size == rank]
+
+    if not my_row_groups:
+        print(f"[R{rank}]: No Row Groups.")
+        return
+
+    print(
+        f"[R{rank}] working on {len(my_row_groups)} row groups divided to batches."
+    )
+
+    for i in range(0, len(my_row_groups), groups_per_batch):
+        batch_groups = my_row_groups[i : i + groups_per_batch]
+        
+        # load just one batch
+        table = parquet_file.read_row_groups(batch_groups)
+        df_batch = table.to_pandas()
+        
+        yield df_batch
