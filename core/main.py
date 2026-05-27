ecd37e9a678c1409f37157e7db5e8d450900dd9c
Leona <zur0037@vsb.cz>
2026-05-28 00:18:07 +0300

    load data into dataloader

--- core/main.py
+++ core/main.py
@@ -5,7 +5,10 @@ import pandas as pd
 import sys
 from pathlib import Path
 import os
-
+import torch
+import torch.distributed as dist
+from datasets import load_dataset
+from torch.utils.data import DataLoader
 _PROJECT_ROOT = Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(_PROJECT_ROOT))
 
@@ -21,17 +24,21 @@ def pprint(text):
 if __name__ == "__main__":
     cfg = load_config("config/my_default.yml")
 
-    df_geo = load_full_parquet(cfg["input"]["file_geo"])
-    pprint(f"Checking df_geo: {df_geo.shape}")
-    if not df_geo.empty:
-        pprint(f"{df_geo.head(2)}")
+    dist.init_process_group(backend="gloo") # Use "nccl" for actual GPU training, "gloo" is safe for CPU/GPU testing
 
-    pprint("Loading shards")
-    GROUPS_PER_BATCH = 1 # set 1 for small batches, 5 for bigger - TODO tunable parameter
-    data_generator = batch_sharded_parquet(cfg["input"]["file_data"], rank, world_size, groups_per_batch=GROUPS_PER_BATCH)
-    pprint("Shard generator loaded")
-    for batch_idx, df_batch_data in enumerate(data_generator):
-            pprint("===============")
-            pprint(f"Working on batch {batch_idx + 1}, shape: {df_batch_data.shape}: {df_batch_data.head(5)}")
-    pprint("Shards generated")
+    dist.barrier()
+    dataset = load_dataset("parquet", data_files=cfg["input"]["file_data"])["train"]
+    sharded_dataset = dataset.shard(num_shards=world_size, index=rank)
+    sharded_dataset = dataset.shard(num_shards=world_size, index=rank)
+    print(f"\n--- [Rank {rank}/{world_size}] Total rows assigned: {len(sharded_dataset)} ---")
+    dataloader = DataLoader(sharded_dataset, batch_size=cfg["nn"]["batch_size"])
+    for batch_idx, batch in enumerate(dataloader):
+        #returns dictionaries of tensors
+        durations = batch["DURATION"]
+        times = batch["TIME"]
 
+        if rank==0:
+            pprint(f"Batch {batch_idx} | Durations: {durations.tolist()} | Times: {times.tolist()}")
+
+    # Clean up the distributed environment
+    dist.destroy_process_group()
